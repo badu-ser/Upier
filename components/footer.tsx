cb8443e6552bf32aa0e43907dea15719204c5b9c
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-11-18 16:14:22 +0530

    Update footer branding and links for X4 eSports

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 const Footer: NextPage = () => {
   return (
     <footer className="footer">
-      <h1 className="logo">U P I E R</h1>
+      <h1 className="logo">X4 ESPORTS</h1>
       <p className="slogan">
-        Shareable Payment&apos;s Link for{" "}
+        X4 eSports Payment&apos;s Link for{" "}
         <img
           src="/images/upi.svg"
           className="upilogo"
@@ -15,12 +15,12 @@ const Footer: NextPage = () => {
         />
       </p>
       <p className="tpf">
-        <Link href={"/privacy-policy"} passHref>
+        <Link href={"https://x4-esports-official.vercel.app/privacy.html"} passHref>
           <u>Privacy Policy</u>
         </Link>
       </p>
-      <p className="tuhin">
-        An Open Source Project by{" "}
+      <p className="X4 eSports India 🇮🇳">
+        A Payment page by{" "}
         <a
           href="http://thetuhin.com/?ref=upier"
           target="_blank"
